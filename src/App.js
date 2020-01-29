3349298d3bdb9b5781cc6a41c0223fd7ad008ea2
keepbang <rlqud1125@naver.com>
2020-01-29 21:43:53 +0900

    Chap 02 commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  
+  render(){
+
+    const text = "리액트 테스트 안녕!";
+    const bool = 1;
+    const style = {
+      backgroundColor : 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      webkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
+      
+    return (
+
+      <Fragment> {/*React v16 이상부터 사용 */}
+        <h1 className="blueTxt">test</h1>
+        <h2 className="blueTxt">{text}</h2>
+        {
+          bool && '참입니다'
+        }
+        <div style={style}></div>
+      </Fragment>
+    );
+  }
+  
 }
 
 export default App;
