c63a946089cd1add9ec4b0914fa9582639527f1f
keepbang <rlqud1125@gmail.com>
2020-02-02 22:55:38 +0900

    chapter 3 props, state

--- src/myComponent/ComponentTest.js
+++ src/myComponent/ComponentTest.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ComponentTest extends Component {
 
+    state = {
+        number : 0
+    }
+
     static defaultProps = {
-        name : '기본값'
+        name : 1
+    }
+
+    static propTypes = {
+        name : PropTypes.number
+    }
+
+    plusBtnEvent = () =>{
+        let num = this.state.number + 1
+        this.setState({
+            number : num
+        })
+        
     }
 
     render() {
+        console.log("component render!!");
         return (
             <div>
-                안녕하세요, 제 이름은 {this.props.name} 입니다.
+                <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+                <p>숫자 : {this.state.number}</p>
+                <button onClick={this.plusBtnEvent}>더하기</button>
             </div>
         );
     }
